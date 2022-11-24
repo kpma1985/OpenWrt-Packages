8e22f7ca9d25d8b5f678dbc0b85818df55797966
楊遠徵 <codes@189.cn>
2022-11-24 22:00:59 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/kinddin9/openwrt-packages
+git clone https://github.com/kiddin9/openwrt-packages
 
 rm -rf openwrt-packages/*.md
 mv -n openwrt-packages/* ./ ; rm -Rf openwrt-packages
