64fcb73c295350fb3e80995c3be88c9c76ecd6a5
楊遠徵 <codes@189.cn>
2022-11-24 22:25:19 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,17 +5,27 @@ git clone https://github.com/kiddin9/openwrt-packages
 rm -rf openwrt-packages/*.md
 mv -n openwrt-packages/* ./ ; rm -Rf openwrt-packages
 
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" *
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*/*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*/*/*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*/*/*/*/*/*
-sed -i "s|https://github.com/kiddin9/|https://msg.ssss.fun#|g" */*/*/*/*/*/*/*/*/*/*
+
+
+
+# Default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' base-files/files/bin/config_generate
+
+# Hostname
+sed -i 's/OpenWrt/SuperNet/g' base-files/files/bin/config_generate
+
+# Timezone
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" base-files/files/bin/config_generate
+
+# Modify banner
+wget --no-check-certificate https://raw.githubusercontent.com/womade/LEDE_actions/main/modify/banner/banner -O base-files/files/etc/banner
+
+# Modify wireless
+sed -i 's/OpenWrt/SuperNet/g' kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+
 
 # EXIT
 rm -rf ./*/.git
