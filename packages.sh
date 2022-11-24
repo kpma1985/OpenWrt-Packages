92a4bd75b6af2fc20c26276ffcea7a9036f8a026
楊遠徵 <codes@189.cn>
2022-11-24 21:49:33 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-git clone https://github.com/kiddin9/openwrt-packages
-rm -rf openwrt-packages/README.md
-mv -n openwrt-packages/* packages ; rm -rf openwrt-packages
+git clone https://github.com/kenzok8/openwrt-packages ops
+git clone https://github.com/kenzok8/small dps
+
+mv -n package/* ./ ; rm -Rf package
+rm -rf ops/README.md
+rm -rf dps/README.md
+mv -n ops/* ./ ; rm -Rf ops
+mv -n dps/* ./ ; rm -Rf dps
+
+# ============================================================
+# PACKAGES END
+# ============================================================
+
+
+
+
+
+# ------------------------------------------------------------
+# MODIFY START
+# ------------------------------------------------------------
+
+# 调整阿里云盘到存储菜单
+sed -i 's/services/nas/g' luci-app-aliyundrive-webdav/luasrc/controller/*.lua
+sed -i 's/services/nas/g' luci-app-aliyundrive-webdav/luasrc/model/cbi/aliyundrive-webdav/*.lua
+sed -i 's/services/nas/g' luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/*.htm
+
+# ------------------------------------------------------------
+# MODIFY END
+# ------------------------------------------------------------
+
 
 # EXIT
 rm -rf ./*/.git
