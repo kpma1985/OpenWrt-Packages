2bc9428be0f67b6d82387ec607cc13ac020fe4e8
楊遠徵 <codes@189.cn>
2022-11-26 18:14:16 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -22,12 +22,11 @@ sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" 
 wget --no-check-certificate https://github.com/womade/LEDE_actions/raw/main/modify/etc/banner -O base-files/files/etc/banner
 
 sed -i "s/%D/SuperNet/g" base-files/files/etc/openwrt_release
-sed -i "s/Kiddin'/Y-ZHENG/g" base-files/files/etc/openwrt_release
+sed -i "s/by Kiddin'/@ Y-ZHENG/g" base-files/files/etc/openwrt_release
 sed -i "s/Kiddin/Y-ZHENG/g" opkg/patches/user_agent.patch
 sed -i "s/Kiddin'/Y-ZHENG/g" luci-app-gpsysupgrade/Makefile
 sed -i "s/Kiddin'/Y-ZHENG/g" my-default-settings/Makefile
 
-rm -rf my-default-settings/files/usr/lib/lua/luci/view/admin_status/index/links.htm
 wget --no-check-certificate https://github.com/womade/OpenWrt-Packages/raw/main/.github/diy/luci/link.html -O my-default-settings/files/usr/lib/lua/luci/view/admin_status/index/links.htm
 
 
